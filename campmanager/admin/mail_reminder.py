f7d152b744c4c261fb2c5eca8a11dbab3c596156
Robert Kaye <rob@musicbrainz.org>
2012-10-06 01:26:24 -0700

    Changed the site encoding to be utf-8. Fixed up the email sending script.

--- campmanager/admin/mail_reminder.py
+++ campmanager/admin/mail_reminder.py
@@ -24,35 +24,38 @@ types = {}
 for code, desc in Group.CAMP_SITE_TYPE_CHOICES:
     types[code] = desc
 
+print "Sending emails:\n"
+
 burners = Burner.objects.all()
 for burner in burners:
-    if burner.user.email != 'mayhem@gmail.com': continue
+    if not burner.user.email:
+        print "user %s has no email set. skipping." % burner.realname
+        continue
 
     groups = Group.objects.filter(user=burner.user.id)
-    msg =  "Hello %s!\n\n" % burner.realname
+    msg =  "Hello %s!\n\n" % burner.realname.encode('utf-8')
     msg += "This email is to remind you that you've signed up to come to the " + \
           "Celebrate Greg party on Oct 13/14.\n\n"
     msg += "You have registered the following group(s):\n\n"
 
     for group in groups:
-        msg += "            name: %s\n" % group.name
-        msg += "     description: %s\n" % group.desc
+        msg += "name: %s\n" % group.name.encode('utf-8')
+        msg += "description: %s\n" % group.desc.encode('utf-8')
         msg += "number of people: %d\n" % group.numpeople
-        msg += " type of camping: %s\n" % types[group.type]
+        msg += "type of camping: %s\n" % types[group.type].encode('utf-8')
         if group.type != Group.OFFSITE_CAMPING:
-            msg += "        sub-camp: %s\n" % group.subcamp.name
+            msg += "sub-camp: %s\n" % group.subcamp.name.encode('utf-8')
 
         msg += "\n"
 
-    msg += "The most important thing is that the NUMBER OF PEOPLE coming is correct. " + \
-          "If the number is NOT correct, please go to http://rsvp.celebrategreg.org TODAY " + \
-          "and update this number to the correct number. If you can no longer attend, set this " + \
-          "number to ZERO.\n\n"
+    msg += "Please verify that the number of people is correct, as well as the type of camping. If these details are " +\
+            "NOT correct, please go to http://rsvp.celebrategreg.org TODAY. Log in using the " + \
+            "same account you used before, and update the information. If you can no longer attend, set the number " + \
+            "of people to ZERO.\n\n"
+
     msg += "If you have any questions about your group registration, please email aleta.dunne@gmail.com . " + \
-          "She will work with you to get your registration squared away.\n\n"
+           "She will work with you to get your registration squared away.\n\n"
     msg += "Thanks for signing up -- we're looking forward to seeing you next weekend!\n\n"
 
-    print msg
-    email("noreply@celebrategreg.org", burner.user.email, "Your Celebrate Greg! group registrations", msg)
-
-
+    print burner.user.email, burner.realname
+    #email("noreply@celebrategreg.org", burner.user.email, "Your Celebrate Greg! group registrations", msg)
